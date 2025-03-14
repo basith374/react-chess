35aa7313a7e9f48d230388d9553a5c5fa0401022
Basith Kunimal <basith374@gmail.com>
2025-03-14 15:13:33 +0530

    convert to vite

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client"; // ✅ Use `react-dom/client` for React 18+
+import "./index.css";
+import App from "./App";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
